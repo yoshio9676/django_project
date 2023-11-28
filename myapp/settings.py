c77ec51cffc43f1334695fc9d655d63eba15e21c
yoshi9676 <yoshitarou0706@gmail.com>
2023-11-28 12:49:16 +0900

    todo app追加

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloworldapp.apps.HelloworldappConfig'
+    'helloworldapp.apps.HelloworldappConfig',
+    'todoapp.apps.TodoappConfig'
 ]
 
 MIDDLEWARE = [
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def helloworldappview(request):
-    return HttpResponse('hello world app')
+def todoList(request):
+    return HttpResponse('todo list app index')
