10e75c328a428143e3a30cdd9592531c649aa6da
yoshi9676 <yoshitarou0706@gmail.com>
2023-11-30 14:45:05 +0900

    todo delete page

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("create/", views.TodoCreate.as_view(), name="todo_create"),
     # 更新
     # 削除
+    path("delete/<int:pk>", views.TodoDelete.as_view(), name="todo_delete"),
     # 詳細
     path('detail/<int:pk>/', views.TodoDetail.as_view(), name="todo_detail"),
     # 一覧
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import TodoModel
 from .forms import TodoForm
 from django.urls import reverse_lazy
@@ -7,7 +7,6 @@ from django.urls import reverse_lazy
 class TodoList(ListView):
     model = TodoModel
     template_name = 'todo/list.html'
-    
     
 class TodoDetail(DetailView):
     model = TodoModel
@@ -17,4 +16,9 @@ class TodoCreate(CreateView):
     model = TodoModel
     form_class = TodoForm
     template_name = 'todo/create.html'
+    success_url = reverse_lazy('todo_list')
+    
+class TodoDelete(DeleteView):
+    model = TodoModel
+    template_name = 'todo/delete.html'
     success_url = reverse_lazy('todo_list')
