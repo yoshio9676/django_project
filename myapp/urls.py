9fb531abc480dd5ba01b0461c9c56107483fb8cd
yoshi9676 <yoshitarou0706@gmail.com>
2023-12-05 16:44:26 +0900

    staticファイルの読み込み

--- myapp/urls.py
+++ myapp/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('helloResponse', helloworldFunction),
     path('jsonResponse', helloJsonResponse)
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
